609aefa91a62cd9f7896d000db16a3f84e737e85
John Lawson <john.rob.lawson@googlemail.com>
2013-09-10 19:40:53 -0500

    Latest update Sep 10

--- lazyWRF.py
+++ lazyWRF.py
@@ -22,20 +22,29 @@ import subprocess
 import calendar
 import math
 import datetime
+import time
 ###########################
 
 # Script SETTINGS
-# If switched on, existing wrfout files etc will be moved to a folder
-move_wrfout = 1
-pathtowrfout = './'
+# If switched on, this will do pre-processing (WPS)
+WPS = 0
+# If switched on, this will do WRF processing
+WRF = 1
 # If switched on, the script will submit jobs (edit as needed)
 submit_job = 1
-pathtoWRF = '../WRFV3/run/'
+
+# If switched on, existing wrfout files etc will be moved to a folder
+move_wrfout = 0
+# Path to WRF folder (absolute)
+pathtoWPS = '/ptmp/jrlawson/WPS/'
+pathtoWRF = '/ptmp/jrlawson/WRFV3/run/'
+# Path to move wrfout* files
+pathtowrfout = '/ptmp/jrlawson/home/path/to/store/wrfout/'
 
 # WRF run SETTINGS
-# Start,end date in YYYYMMDD_HHMM
-idate = '20060526_0000'
-fdate = '20060527_1200'
+# Start,end date in (YYYY,MM,DD,H,M,S)
+idate = (2006,05,26,0,0,0)
+fdate = (2006,05,27,12,0,0)
 domains = 1
 e_we = (500,) # Needs to be same length as number of domains
 e_sn = (500,)
@@ -46,12 +55,12 @@ j_start = (1,)
 parent_grid_ratio = (1,) # Same length as num of domains; ratio of each parent to its child
 
 # Select initial data source
-# gfs, nam, gefs
+# FROM: gfs, nam, (gefs --> use setup_gefs.py script for now)
 init_data = 'gfs'
 # Directory with initialisation data
-pathtoinitdata = './gfsfiles/gfsanl'
-# Intermediate file prefix
-prefix = "FILE"
+pathtoinitdata = './gfsfiles/'
+# Intermediate file prefix (usually no need to change)
+int_prefix = "FILE"
 
 ############################
 
@@ -69,6 +78,7 @@ def edit_namelist(old,new,incolumn=1):
             nameout.writelines(nwps)
             nameout.close()
             break
+    return
 
 def edit_namelist_input(old,new,pathtoWRF=pathtoWRF):
     ninput = open(pathtoWRF+'namelist.input','r').readlines()
@@ -80,6 +90,82 @@ def edit_namelist_input(old,new,pathtoWRF=pathtoWRF):
             nameout.writelines(ninput)
             nameout.close()
             break
+    return
+
+def str_from_date(date,format):
+    # date = Input is tuple (yyyy,mm,dd,h,m,s)
+    # format = choose 'list' or 'indiv' or...
+    # ... year, month, day, hour, minute, second output
+    datelist = []
+    for n in date:
+        datelist.append("%02u" %n)
+    if format=='list':
+        return datelist
+    else:
+        year,month,day,hour,minute,second = datelist
+        if format=='indiv':
+            return year,month,day,hour,minute,second
+        else:
+            val = eval(format)
+            return val
+
+def download_data(date,initdata,pathtoinitdata):
+    # Download gfs, nam data from server for a timestamp
+    if initdata == 'gfs':
+        prefix = 'gfsanl'
+        n = '3'
+        suffix = '.grb'
+    elif initdata == 'nam':
+        prefix = 'namanl'
+        n = '218'
+        suffix = '.grb'
+    command = ('wget "http://nomads.ncdc.noaa.gov/data/' + prefix + '/' + date[:6] + '/' + date[:8] + 
+                '/' + prefix + '_' + n + '_' + date + '_000' + suffix + '" -P ' + pathtoinitdata)
+    #pdb.set_trace()
+    os.system(command)
+    return
+
+def get_init_files(initdata,idate,fdate,pathtoinitdata):
+    # Let's assume all initdata possibilities start at 0000 UTC and the interval is an integer factor
+    if initdata=='gfs':
+        interval = 6.0
+        prefix = 'gfsanl'
+    elif initdata=='nam':
+        interval = 6.0
+        prefix = 'namanl'
+
+    # First, convert dates to seconds-from-epoch time
+    idaten = calendar.timegm(idate)
+    fdaten = calendar.timegm(fdate)
+    
+    # The initial and final files needed:
+    ifiledate = idaten - idaten%(interval*3600)
+    ffiledate = fdaten - fdaten%(interval*3600) + (interval*3600)
+
+    # Create a range of required file dates
+    required_dates = range(int(ifiledate),int(ffiledate),int(interval*3600))
+
+    # List all files in initialisation data folder
+    initfiles = glob.glob(pathtoinitdata + '*')
+
+    # Loop through times and check to see if file exists for initialisation model
+    for r in required_dates:
+        # Tuple of date (9 items long starting with year, month, etc...)
+        longdate = time.gmtime(r)
+        fname_date = ''.join(["%02u" %x for x in longdate[0:3]]) + '_' + ''.join(["%02u" %x for x in longdate[3:5]])
+        checkfiles_prefix = []
+        checkfiles_date = []
+        for f in initfiles:
+            checkfiles_prefix.append(prefix in f)
+            checkfiles_date.append(fname_date in f)
+        try:
+            checkfiles_prefix.index(1) + checkfiles_date.index(1)
+        except ValueError:
+            print 'Downloading required file for ' + fname_date
+            download_data(fname_date,init_data,pathtoinitdata)
+        else:     
+            print 'Data for ' + fname_date + ' already exists.'
+    return
 
 ############################
 ############################
@@ -94,56 +180,72 @@ if init_data == 'gfs':
     soil_levs = 4
     interval = 6 # In hours
     Vtable_suffix = 'GFS'
+    init_prefix = 'gfsanl'
 elif init_data == 'nam':
     atmos_levs = 40
     soil_levs = 4
     interval = 6 # In hours
     Vtable_suffix = 'NAM'
-elif init_data == 'gefs':
-    ens = raw_input('Which ensemble member? (c00,p01...p10) ')
-    atmos_levs = 12
-    soil_levs = 4 # Uses GFS soil data
-    interval = 3 # Interpolation happens after 90 h
-    Vtable_suffix = 'GEFSR2'
+    init_prefix = 'namanl'
+elif init_data == 'gefs': # WORK IN PROGRESS
+    #ens = raw_input('Which ensemble member? (c00,p01...p10) ')
+    #atmos_levs = 12
+    #soil_levs = 4 # Uses GFS soil data
+    #interval = 3 # Interpolation happens after 90 h
+    #Vtable_suffix = 'GEFSR2'
+    print 'Work in progress - check back later.'
 
-# Prepares namelist.wps
-idate_s = "'"+idate[:4]+"-"+idate[4:6]+"-"+idate[6:11]+":"+idate[11:13]+":00',"
-edit_namelist("start_date",idate_s * domains, incolumn=0)
-fdate_s = "'"+fdate[:4]+"-"+fdate[4:6]+"-"+fdate[6:11]+":"+fdate[11:13]+":00',"
-edit_namelist("end_date",fdate_s * domains, incolumn=0)
-edit_namelist("max_dom",str(domains)+',',incolumn=0)
-edit_namelist("interval_seconds",str(interval*3600)+',', incolumn=0)
-edit_namelist("parent_grid_ratio",', '.join([str(p) for p in parent_grid_ratio])+',')
-edit_namelist("i_parent_start", ', '.join([str(i) for i in i_start])+',')
-edit_namelist("j_parent_start", ', '.join([str(j) for j in j_start])+',')
-edit_namelist("dx",str(dx*1000),incolumn=0)
-edit_namelist("dy",str(dy*1000),incolumn=0)
-edit_namelist("e_we",', '.join([str(w) for w in e_we])+',')
-edit_namelist("e_sn",', '.join([str(s) for s in e_sn])+',')
-edit_namelist("prefix",prefix+',',incolumn=0)
-edit_namelist("fg_name",prefix+',',incolumn=0)
+# Get nice strings for namelist writing
+y1,mth1,d1,h1,min1,s1 = str_from_date(idate,'indiv')
+y2,mth2,d2,h2,min2,s2 = str_from_date(fdate,'indiv')
 
-# Add your own here if wanting to change e.g. domain location, dx, dy from here...    
+if WPS:
+    # Prepares namelist.wps
+    idate_s = "'"+y1+"-"+mth1+"-"+d1+'_'+h1+':'+min1+":"+s1+"',"
+    edit_namelist("start_date",idate_s * domains, incolumn=0)
+    fdate_s = "'"+y2+"-"+mth2+"-"+d2+'_'+h2+':'+min2+":"+s2+"',"
+    edit_namelist("end_date",fdate_s * domains, incolumn=0)
+    edit_namelist("max_dom",str(domains)+',',incolumn=0)
+    edit_namelist("interval_seconds",str(interval*3600)+',', incolumn=0)
+    edit_namelist("parent_grid_ratio",', '.join([str(p) for p in parent_grid_ratio])+',')
+    edit_namelist("i_parent_start", ', '.join([str(i) for i in i_start])+',')
+    edit_namelist("j_parent_start", ', '.join([str(j) for j in j_start])+',')
+    edit_namelist("dx",str(dx*1000),incolumn=0)
+    edit_namelist("dy",str(dy*1000),incolumn=0)
+    edit_namelist("e_we",', '.join([str(w) for w in e_we])+',')
+    edit_namelist("e_sn",', '.join([str(s) for s in e_sn])+',')
+    edit_namelist("prefix",int_prefix+',',incolumn=0)
+    edit_namelist("fg_name",int_prefix+',',incolumn=0)
 
-# Link to, and ungrib, initialisation files
-os.system('./link_grib.csh ' + pathtoinitdata)
-os.system('ln -sf ungrib/Variable_Tables/Vtable.' + Vtable_suffix + ' Vtable')
-os.system('./ungrib.exe')
-os.system('./metgrid.exe')
+    # Add your own here if wanting to change e.g. domain location, dx, dy from here...    
+
+    # Run geogrid
+    os.system('./geogrid.exe')
+
+    # Check to see if initialisation files exist
+    # If they don't, download into data directory
+    get_init_files(init_data,idate,fdate,pathtoinitdata)
+
+    # Link to, and ungrib, initialisation files
+    os.system('./link_grib.csh ' + pathtoinitdata + init_prefix)
+    os.system('ln -sf ungrib/Variable_Tables/Vtable.' + Vtable_suffix + ' Vtable')
+    os.system('./ungrib.exe')
+    os.system('./metgrid.exe')
 
 # Submit jobs (edit as needed)
-if submit_job == 1:
+if WRF:
     # Soft link data netCDFs files from WPS to WRF
-    os.system('ln -sf ./met_em* ' + pathtoWRF)
+    os.system('ln -sf ' + pathtoWPS + 'met_em* ' + pathtoWRF)
     
     ##### Sync namelist.input with namelist.wps
     # Copy original in case of bugs
     os.system('cp ' + pathtoWRF + 'namelist.input{,.python_backup}')
+    
+    print 'met_em* linked. Now amending namelist.input.'
+
     # Compute run time
-    dt_1 = calendar.timegm((int(idate[0:4]),int(idate[4:6]),int(idate[6:8]),
-                            int(idate[9:11]),int(idate[11:13]),0))
-    dt_2 = calendar.timegm((int(fdate[0:4]),int(fdate[4:6]),int(fdate[6:8]),
-                            int(fdate[9:11]),int(fdate[11:13]),0))
+    dt_1 = calendar.timegm(idate)
+    dt_2 = calendar.timegm(fdate)
     dt = datetime.timedelta(seconds=(dt_2 - dt_1))
     days = dt.days
     hrs = math.floor(dt.seconds/3600.0)
@@ -167,18 +269,18 @@ if submit_job == 1:
     edit_namelist_input("run_hours","%01u" %hrs)
     edit_namelist_input("run_minutes","%01u" %mins)
     edit_namelist_input("run_seconds","%01u" %secs)
-    edit_namelist_input("start_year", (idate[:4]+', ')*domains)
-    edit_namelist_input("start_month", (idate[4:6]+', ')*domains)
-    edit_namelist_input("start_day", (idate[6:8]+', ')*domains)
-    edit_namelist_input("start_hour", (idate[9:11]+', ')*domains)
-    edit_namelist_input("start_minute", (idate[11:13]+', ')*domains)
-    edit_namelist_input("start_second", ('00, ')*domains)
-    edit_namelist_input("end_year", (fdate[:4]+', ')*domains)
-    edit_namelist_input("end_month", (fdate[4:6]+', ')*domains)
-    edit_namelist_input("end_day", (fdate[6:8]+', ')*domains)
-    edit_namelist_input("end_hour", (fdate[9:11]+', ')*domains)
-    edit_namelist_input("end_minute", (fdate[11:13]+', ')*domains)
-    edit_namelist_input("end_second", ('00, ')*domains)
+    edit_namelist_input("start_year", (y1+', ')*domains)
+    edit_namelist_input("start_month", (mth1+', ')*domains)
+    edit_namelist_input("start_day", (d1+', ')*domains)
+    edit_namelist_input("start_hour", (h1+', ')*domains)
+    edit_namelist_input("start_minute", (min1+', ')*domains)
+    edit_namelist_input("start_second", (s1+', ')*domains)
+    edit_namelist_input("end_year", (y2+', ')*domains)
+    edit_namelist_input("end_month", (mth2+', ')*domains)
+    edit_namelist_input("end_day", (d2+', ')*domains)
+    edit_namelist_input("end_hour", (h2+', ')*domains)
+    edit_namelist_input("end_minute", (min2+', ')*domains)
+    edit_namelist_input("end_second", (s2+', ')*domains)
     edit_namelist_input("interval_seconds", str(interval*3600)+',')
     edit_namelist_input("max_dom",str(domains)+',')
     edit_namelist_input("e_we", ', '.join([str(w) for w in e_we])+',')
@@ -190,18 +292,22 @@ if submit_job == 1:
     edit_namelist_input("j_parent_start", ', '.join([str(j) for j in j_start])+',')
     edit_namelist_input("parent_grid_ratio",', '.join([str(p) for p in parent_grid_ratio])+',') 
 
-    """
-    # Run real, get ID number of job
-    # Change name of submission script if needed
-    p_real = subprocess.call([qsub,real_run.sh],cwd=pathtoWRF,shell=True,
-                            stdout=subprocess.PIPE)
-    jobid = p.stdout.read()[:5] # Assuming last five digits = job ID.
-    # Run WRF but wait until Real has finished without errors
-    # Again, change name of submission script if needed
-    p_wrf = subprocess.call([qsub,wrf_run.sh,-W,"afterok=jobid"],
-                            cwd=pathtoWRF,shell=True)
-    print "real.exe and wrf.exe submitted. Exiting Python script."
-    """
-else:
-    print "Pre-processing complete. Exiting Python script."
+    
+    if submit_job:
+        print 'Namelist edited. Now submitting real.exe.'  
+        # Run real, get ID number of job
+        # Change name of submission script if needed
+        p_real = subprocess.Popen('qsub -d'+pathtoWRF+' real_run.sh',cwd=pathtoWRF,shell=True,stdout=subprocess.PIPE)
+        p_real.wait()
+        print p_real.stdout.read()
+        jobid = p_real.stdout.read()[:5] # Assuming last five digits = job ID.
+        # Run WRF but wait until Real has finished without errors
+        print 'Now submitting wrf.exe.'  
+        # Again, change name of submission script if needed
+        p_wrf = subprocess.Popen('qsub -d'+pathtoWRF+' wrf_run.sh -W afterok=jobid',cwd=pathtoWRF)
+        p_wrf.wait()
+        print "real.exe and wrf.exe submitted. Exiting Python script."
+
+    else:
+        print "Pre-processing complete. Exiting Python script."
 
