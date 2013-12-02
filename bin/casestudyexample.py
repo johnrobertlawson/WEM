e344baf7dcafab5534ef76e849ab16d4acc5b2d0
John Lawson <john.rob.lawson@googlemail.com>
2013-12-02 10:30:59 -0600

    Organising the settings module.

--- bin/casestudyexample.py
+++ bin/casestudyexample.py
@@ -4,13 +4,7 @@ The class PyWRFSettings is used to create config, containing all settings
 """
 
 from PyWRFPlus import PyWRFPlusEnv
-
-class PyWRFSettings:
-    def __init__(self):
-        # Settings go here
-        self.DPI = 250.0
-        self.output_rootdir = '/tera9/jrlawson/test/'
-        self.wrfout_rootdir = '/tera9/jrlawson/bowecho/'
+from settings import PyWRFSettings
 
 config = PyWRFSettings()
 PyWRFPlusEnv(config)
---
+++ bin/settings.py
@@ -0,0 +1,8 @@
+class PyWRFSettings:
+    def __init__(self):
+        # Required settings:
+        self.output_rootdir = '/tera9/jrlawson/test/'
+        self.wrfout_rootdir = '/tera9/jrlawson/bowecho/'
+        # Optional settings:
+        self.DPI = 250.0
+
