20dbb1cfc6a8b465e90d38c64868cab55627a7a5
John Lawson <john.rob.lawson@googlemail.com>
2014-01-06 10:47:41 -0700

    DKE work

--- postWRF/bin/DKE.py
+++ postWRF/bin/DKE.py
@@ -3,16 +3,26 @@ of data stored to disc. This is then plotted using the package.
 """
 
 from DKE_settings import Settings
+from ../postWRF import WRFEnviron
 
 # Initialise settings and environment
 config = Settings()
-p = PyWRFEnv(config)
+p = WRFEnviron(config)
 
 # User settings
+init_time = p.string_from_time('dir',(2009,9,10,0,0,0),strlen='hour')
+rootdir = '/uufs/chpc.utah.edu/common/home/horel-group2/lawson2/'
+outdir = '/uufs/chpc.utah.edu/common/home/u0737349/public_html/paper2/'
 
+for rundate in ('25','27','29'):
+    foldername = '201111' + rundate + '00'
+    runfolder = os.path.join(rootdir,foldername)
+    path_to_wrfouts = p.wrfout_files_in(wrfout_dir,dom=1)
 
-# Produce .npy data files with DKE data
+    path_to_plots = os.path.join(outdir,runfolder)
 
-
-# Plot these .npy files
-p.plot2D('DKE_2D',path_to_data,path_to_plots)
+    # Produce .npy data files with DKE data
+    p.compute_diff_energy('sum_z','kinetic',path_to_wrfouts,times,upper=500,
+                          d_save=runfolder, d_return=0,d_fname='DKE_'+foldername)
+    # Plot these .npy files
+    p.plot_diff_energy('sum_z',times,runfolder,path_to_plots)
