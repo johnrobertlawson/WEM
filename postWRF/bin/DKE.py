23919df95d883cd6787b96594619f96cff1b2493
John Lawson <john.rob.lawson@googlemail.com>
2014-01-16 12:32:27 -0700

    Polishing DKE/DTE computations

--- postWRF/bin/DKE.py
+++ postWRF/bin/DKE.py
@@ -25,7 +25,7 @@ rootdir = '/uufs/chpc.utah.edu/common/home/horel-group2/lawson2/'
 outdir = '/uufs/chpc.utah.edu/common/home/u0737349/public_html/paper2/'
 
 #for rundate in ('25','27','29'):
-for rundate in ['29']:
+for rundate in ['25','27']:
     print("Computing for {0} November".format(rundate))
     foldername = '201111' + rundate + '00'
     runfolder = os.path.join(rootdir,foldername)
@@ -33,15 +33,13 @@ for rundate in ['29']:
 
     itime = (2011,11,int(rundate),0,0,0)
     ftime = (2011,12,2,12,0,0)
-    times = p.generate_times(itime,ftime,6*3600)
+    times = p.generate_times(itime,ftime,12*3600)
     path_to_plots = os.path.join(outdir,foldername)
     #pdb.set_trace()
     # Produce .npy data files with DKE data
     print("Compute_diff_energy...")
-    """
     p.compute_diff_energy('sum_z','kinetic',path_to_wrfouts,times,upper=500,
                               d_save=runfolder, d_return=0,d_fname='DKE_500_'+foldername)
-    """
     # Contour fixed at these values
     V = range(0,2200,200)
     p.plot_diff_energy('sum_z','kinetic',times,runfolder,'DKE_500_'+foldername,path_to_plots,V)
