0d30ab7afed19bf6b79a27f1237965fc0c820731
John Lawson <john.rob.lawson@googlemail.com>
2014-02-25 09:13:22 -0600

    Bugfixes

--- postWRF/bin/20110419_plot.py
+++ postWRF/bin/20110419_plot.py
@@ -9,9 +9,14 @@ from settings import Settings
 config = Settings()
 p = WRFEnviron(config)
 
+case = '20110419'
+IC = 'GEFSR2' 
+ensnames = ['c00'] + ['p'+"%02d" %n for n in range(1,11)]
+experiment = {'ICBC':'CTRL'}
+
 #variables = {'wind10':2000}
 #variables = {'cref':2000}
-variables = {'cref':2000,'wind10':2000}
+variables = {'cref':2000,'wind10':2000,'CAPE':2000}
 #variables = {'wind':'sfc','cref':'na','thetae':850]
 #variables = {'thetae':850}
 
@@ -19,5 +24,8 @@ itime = (2011,4,19,16,0,0)
 ftime = (2011,4,20,11,30,0)
 times = p.generate_times(itime,ftime,60*60)
 
-p.plot_2D(variables,times)
-#p.plot_upper_variable('thetae',850,times)
+for en in ensnames:
+    addpath = os.path.join(p.output_root,case,IC,en,experiment.keys()[0])
+    p.output_root += addpath
+    p.wrfout_root += addpath
+    p.plot_2D(variables,times)
--- postWRF/bin/settings.py
+++ postWRF/bin/settings.py
@@ -1,7 +1,7 @@
 class Settings:
     def __init__(self):
-        self.output_root = '/home/jrlawson/public_html/bowecho/20110419/GEFSR2/p03/'
-        self.wrfout_root = '/chinook2/jrlawson/bowecho/20110419/GEFSR2/p03/'
+        self.output_root = '/home/jrlawson/public_html/bowecho/'
+        self.wrfout_root = '/chinook2/jrlawson/bowecho/'
         self.p_interp_root = '/home/jrlawson/fortrancode/P_INTERP/'
         self.DPI = 100.0
         self.plot_titles = 1
