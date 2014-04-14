59fedc420ee8d1486b1e0a203e78e6b23c6e899c
John Lawson <john.rob.lawson@googlemail.com>
2014-04-14 15:07:45 -0500

    Updates..

--- postWRF/bin/DKE.py
+++ postWRF/bin/DKE.py
@@ -18,9 +18,9 @@ import WEM.utils.utils as utils
 #case = '20110419'
 case = '20130815'
 
-IC = 'NAM'
-experiment = 'MXMP'
-ens = 'anl'
+IC = 'GEFSR2'
+experiment = 'STCH'
+ens = 'p09'
 # Time script
 scriptstart = time.time()
 
@@ -47,11 +47,11 @@ ftime = (2013,8,16,12,0,0)
 times = utils.generate_times(itime,ftime,3*3600)
 config.output_root = os.path.join(config.output_root,case,IC)
 #pdb.set_trace()
-#p.compute_diff_energy('sum_z','kinetic',path_to_wrfouts,times,
-#                          d_save=runfolder, d_return=0,d_fname='DTE_MXMP')
+p.compute_diff_energy('sum_z','total',path_to_wrfouts,times,
+                          d_save=runfolder, d_return=0,d_fname='DTE_STCH')
 
 # Contour fixed at these values
 #V = range(500,5000,250)
-p.plot_diff_energy('sum_xyz','total',times,runfolder,'DTE_MXMP',config.output_root,V)
+p.plot_diff_energy('sum_z','total',times,runfolder,'DTE_STCH',config.output_root,V)
 
 print "Script took", time.time()-scriptstart, "seconds."
