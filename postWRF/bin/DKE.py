7ca796685ba34a0a8545a11608a9fd1db806ae18
John Lawson <john.rob.lawson@googlemail.com>
2014-04-16 13:55:35 -0500

    DKE controlling script updated

--- postWRF/bin/DKE.py
+++ postWRF/bin/DKE.py
@@ -19,12 +19,12 @@ import WEM.utils.utils as utils
 case = '20130815'
 
 IC = 'GEFSR2'
-experiment = 'MXMP'
+experiment = 'STCH'
 ens = 'p09'
-# MP = 'Morrison_Hail'
+MP = 'Morrison_Hail'
 # Time script
 scriptstart = time.time()
-stoch_ens = ["s{0:02d}".format(n) for n in range(1,11)]
+stoch_names = ["s{0:02d}".format(n) for n in range(1,11)]
 MPs = ('ICBC','WSM6_Grau','WSM6_Hail','Kessler','Ferrier','WSM5',
 		'WDM5','Lin','WDM6_Grau','WDM6_Hail',
 		'Morrison_Grau','Morrison_Hail')
@@ -32,7 +32,6 @@ MPs = ('ICBC','WSM6_Grau','WSM6_Hail','Kessler','Ferrier','WSM5',
 # Initialise settings and environment
 config = Settings()
 p = WRFEnviron(config)
-
 
 
 #itime = (2006,5,26,0,0,0)
@@ -50,15 +49,15 @@ ftime = (2013,8,16,12,0,0)
 times = utils.generate_times(itime,ftime,3*3600)
 #members = ['s0{0}'.format(n) for n in range(1,8)]
 
-# for s in members:
-fpath = os.path.join(config.wrfout_root,case,IC,ens)
-path_to_wrfouts = p.wrfout_files_in(fpath,dom=1,avoid=stoch_ens)
+path_to_wrfouts = []
+for s in stoch_names:
+    fpath = os.path.join(config.wrfout_root,case,IC,ens,MP,s)
+    path_to_wrfouts.append(p.wrfout_files_in(fpath,dom=1)[0])
 
-picklefolder = fpath
-p.C.output_root = os.path.join(config.output_root,case,IC,ens)
+picklefolder = os.path.join(config.wrfout_root,case,IC,ens,MP)
+p.C.output_root = os.path.join(config.output_root,case,IC,ens,MP)
 pfname = 'DTE_' + experiment
 
-"""
 # pdb.set_trace()
 p.compute_diff_energy('sum_z','total',path_to_wrfouts,times,
                           d_save=picklefolder, d_return=0,d_fname=pfname)
@@ -66,9 +65,9 @@ p.compute_diff_energy('sum_z','total',path_to_wrfouts,times,
 # Contour fixed at these values
 V = range(250,5250,250)
 VV = [100,] + V
-p.plot_diff_energy('sum_z','total',times,picklefolder,pfname,VV,)
+p.plot_diff_energy('sum_z','total',times,picklefolder,pfname,VV)
+
+#p.plot_growth_sensitivity('DTE',picklefolder,pfname,MPs)
 
 print "Script took", time.time()-scriptstart, "seconds."
 
-"""
-p.plot_growth_sensitivity('DTE',picklefolder,pfname,MPs)
