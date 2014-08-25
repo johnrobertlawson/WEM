1adc932c2e9bd8340cf6c061ccb7e735bae059d0
John Lawson <john.rob.lawson@googlemail.com>
2014-08-24 22:24:36 -0500

    Added paramiko to utils

--- utils/unix_tools.py
+++ utils/unix_tools.py
@@ -4,8 +4,12 @@ Utility scripts to help with directory, file, etc issues
 
 import os
 import GIS_tools as utils
+try:
+    import paramiko
+except ImportError:
+    print("Module paramiko unavailable.")
 
-def dir_from_naming(self,root,*args):
+def dir_from_naming(root,*args):
     """
     Generate file path from arguments
 
@@ -17,3 +21,13 @@ def dir_from_naming(self,root,*args):
     path = os.path.join(root,*l)
     return path
 
+def ssh_client(domain,username,password):
+    #key = paramiko.RSAKey(data=base64.decodestring('AAA...'))
+    client = paramiko.SSHClient()
+    #client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
+    client.connect(domain, username=username, password=password)
+    return client
+    stdin, stdout, stderr = client.exec_command('ls')
+    for line in stdout:
+        print '... ' + line.strip('\n')
+    client.close()
